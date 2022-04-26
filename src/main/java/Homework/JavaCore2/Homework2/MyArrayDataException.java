9e573829077d53a7f3305c5c27a270d4108c47a6
Schizovoid <IZhemerev@gmail.com>
2022-04-27 01:23:00 +0300

    Homework 2.2 done

---
+++ src/main/java/Homework/JavaCore2/Homework2/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package Homework.JavaCore2.Homework2;
+
+public class MyArrayDataException extends RuntimeException {
+    public MyArrayDataException(){}
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/Homework/JavaCore2/Homework2/MySizeArrayException.java
@@ -0,0 +1,8 @@
+package Homework.JavaCore2.Homework2;
+
+public class MySizeArrayException extends RuntimeException {
+    public MySizeArrayException (){}
+    public MySizeArrayException (String message){
+        super (message);
+    }
+}
