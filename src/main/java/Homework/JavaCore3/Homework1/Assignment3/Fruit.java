dcce93e4afc52de0c1d292dc849f15f06b923a19
Ivan <IZhemerev@gmail.com>
2022-08-20 22:15:42 +0300

    Homework 3.1

---
+++ src/main/java/Homework/JavaCore3/Homework1/Assignment3/Fruit.java
@@ -0,0 +1,8 @@
+package Homework.JavaCore3.Homework1.Assignment3;
+
+public abstract class Fruit {
+    Float weight;
+    public Float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/Homework/JavaCore3/Homework1/Assignment3/Orange.java
@@ -0,0 +1,8 @@
+package Homework.JavaCore3.Homework1.Assignment3;
+
+public class Orange extends Fruit {
+    public Orange() {
+        super();
+        this.weight = 1.5f;
+    }
+}
